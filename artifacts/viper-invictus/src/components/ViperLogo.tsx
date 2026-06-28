dc27810bf0b45290938d4484afa73f22844063be
Viper Team <devinaiisthebest272829@gmail.com>
2026-06-28 09:31:43 +0800

    Cache-bust logo with query param

--- artifacts/viper-invictus/src/components/ViperLogo.tsx
+++ artifacts/viper-invictus/src/components/ViperLogo.tsx
@@ -3,7 +3,7 @@ import logoSrc from "@/assets/viper-logo.png";
 export function ViperLogo({ className = "", size }: { className?: string; size?: number }) {
   return (
     <img
-      src={logoSrc}
+      src={`${logoSrc}?v=2`}
       alt="Viper Invictus"
       className={className}
       width={size}
